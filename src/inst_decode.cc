237af3f28b2d6e38e9dfde39c8ad52ecf8bf4ae2
Alex Midlash <amidlash@gmail.com>
2015-06-28 02:11:24 -0500

    added logic to decode a few more loads

--- src/inst_decode.cc
+++ src/inst_decode.cc
@@ -1,31 +1,14 @@
-/*
-  MSB ..... LSB
-
-  registers:
-  000 - b
-  001 - c
-  010 - NOT USED
-  011 - e
-  100 - h
-  101 - d ? SAME?!??!
-  101 - l
-  110 - RESERVED, see instruction formats
-  111 - a
-  LD - p85
-  LD : 00 R N
-  LD : 01 R R'
-*/
-
 #include <stdint.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 enum reg_encodings {
   REG_b = 0,
   REG_c = 1,
-  REG_UNUSED = 2,
+  REG_2 = 2,
   REG_e = 3,
   REG_h = 4,
   REG_d = 5,
-  REG_RESV = 6,
+  REG_6 = 6,
   REG_a = 7
 };
 
@@ -40,8 +23,31 @@ uint8_t select_bits(uint8_t in, char start, char end) {
   return (in & ones) >> end;
 }
 
-void decode_instruction(char *rom, bool listOnly = true) {
-  uint8_t instr = (*rom) & MASK_76;
-  uint8_t op1 = (*rom) & MASK_543;
-  uint8_t op2 = (*rom) & MASK_210;
+unsigned char *decode_instruction(unsigned char *rom, bool listOnly = true) {
+  uint8_t instr = select_bits(*rom, 7, 6);
+  uint8_t op1 = select_bits(*rom, 5, 3);
+  uint8_t op2 = select_bits(*rom, 2, 0);
+  printf("i=%d\top1=%d\top2=%d\n", instr, op1, op2);
+  switch(instr) {
+  case 0:
+    if(op1 == REG_6 && op2 == REG_6)
+      puts("LD (HL) N");
+    else if(op2 == REG_6)
+      puts("LD R N");
+    else if(op1 == REG_a && op2 == REG_2)
+      puts("LD A (BC)");
+    break;
+  case 1:
+    if(op1 == REG_6)
+      puts("LD (HL) R");
+    else if(op2 == REG_6)
+      puts("LD R (HL)");
+    else
+      puts("LD R R");
+    break;
+  default:
+    puts("unable to decode instruction");
+    exit(-1);
+  }
+  return ++rom;
 }
--- src/inst_decode_test.cc
+++ src/inst_decode_test.cc
@@ -16,11 +16,17 @@
   }
 
 int main(void) {
-  TEST_CATEGORY("instruction decoding");
-  TEST_EQ(select_bits(0x1, 0, 0), 1)
-  TEST_EQ(select_bits(0xf2, 1, 0), 2)
-  TEST_EQ(select_bits(0xf2, 7, 7), 1)
-  TEST_EQ(select_bits(0xf2, 7, 6), 3)
+    TEST_CATEGORY("instruction decoding");
+    TEST_EQ(select_bits(0x1, 0, 0), 1);
+    TEST_EQ(select_bits(0xf2, 1, 0), 2);
+    TEST_EQ(select_bits(0xf2, 7, 7), 1);
+    TEST_EQ(select_bits(0xf2, 7, 6), 3);
+
+    unsigned char instr[] = {0b01111001, // LD A, C
+                             0b00111110, // LD A, N
+    };
+    for(int i=0;i<2;i++)
+      decode_instruction(instr+i, false);
 
     return 0;
 }
